d43f165b2865d79b992e37fe6e7ad575b8806cd5
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-22 15:24:00 -0700

    implemented signout method and route

--- controllers/user.js
+++ controllers/user.js
@@ -47,3 +47,11 @@ exports.signin = (req, res) => {
     return res.json({token, user: {_id, email, name, role}})
   });
 };
+
+exports.signout = (req, res) => {
+  // here we need to clear cookie that we had in signin method
+  res.clearCookie('t');
+  res.json({
+    message: 'Signout success' 
+  });
+};
