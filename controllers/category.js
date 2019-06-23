f4ae1fdc096a1b21671a148f4eceda3e2584fb63
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-23 12:01:06 -0700

    imolemented delete, update, and get all for category

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,21 @@
 const Category = require ("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "Category does not exist"
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
+//============================CREATE=====================================
+
 exports.create = (req, res) => {
   const category = new Category(req.body)
   category.save((err, data) => {
@@ -12,3 +27,55 @@ exports.create = (req, res) => {
     res.json({ data })
   })
 }
+
+//============================READ=====================================
+
+exports.read = (req, res) => {
+  console.log('hi');
+  console.log(req.category);
+  return res.json(req.category);
+};
+
+//============================UPDATE=====================================
+
+exports.update = (req, res) => {
+  const category = req.category
+  category.name = req.body.name
+  category.save((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(data);
+  });
+};  
+
+//============================REMOVE=====================================
+
+exports.remove = (req, res) => {
+  const category = req.category
+  category.remove((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: 'Category deleted'
+    });
+  });
+}; 
+
+//============================READ ALL=====================================
+
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(data);
+  });
+};
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
+//=====================READ===================
+
+router.get('/category/:categoryId', read);
+
+//=====================WRITE==================
 
 router.post(
   '/category/create/:userId',
@@ -14,6 +19,34 @@ router.post(
    create
    );
 
-router.param('userId', userById)
+//=====================UPDATE===================
+
+  router.put(
+  '/category/:categoryId/:userId',
+    requireSignin,
+    isAuth, 
+    isAdmin, 
+    update
+    );
+
+//=====================DELETE===================     
+
+
+  router.delete(
+    '/category/:categoryId/:userId',
+    requireSignin,
+    isAuth, 
+    isAdmin, 
+    remove
+    );
+
+//====================READ ALL====================
+
+   router.get('/categories', list);
+
+//=================================================
+
+router.param('categoryId', categoryById);
+router.param('userId', userById);
 
 module.exports = router;  
