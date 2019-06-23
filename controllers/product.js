611d21c6d7e83ebbde98b6fdcbf68a4fbaa02991
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-22 23:45:24 -0700

    implemented productById middleware

--- controllers/product.js
+++ controllers/product.js
@@ -4,8 +4,24 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = ( req, res, next, id ) => {
+  Product.findById(id).exec((err, product) => {
+    if(err || !product) {
+      return res.status(400).json({
+        error: "Product not found"
+      });
+    }
+    req.product = product
+    next();
+  });
+}
 
-exports.create = ( req, res) => {
+exports.read = (req, res) => {
+  req.product.photo = undefined
+  return res.json(req.product);
+};
+
+exports.create = (req, res) => {
   let form = new formidable.IncomingForm()
   form.keepExtensions = true
   form.parse(req, (err, fields, files) => {
