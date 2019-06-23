95a97103311750bbcc9872a69498e9d49f05c88e
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-23 10:34:33 -0700

    created product update

--- controllers/product.js
+++ controllers/product.js
@@ -16,10 +16,14 @@ exports.productById = ( req, res, next, id ) => {
   });
 }
 
+//==========================READ===============================
+
 exports.read = (req, res) => {
   req.product.photo = undefined
   return res.json(req.product);
 };
+
+//==========================WRITE===============================
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm()
@@ -81,6 +85,8 @@ exports.create = (req, res) => {
   });
 };
 
+//==========================DELETE===============================
+
 exports.remove = (req, res) => {
   let product = req.product
   product.remove((err, deleteProduct) => {
@@ -92,5 +98,64 @@ exports.remove = (req, res) => {
     res.json({
       message: 'Product deleted successfully'
     });
+  });
+};
+
+//==========================UPDATE===============================
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded'
+      });
+    }
+    //check for all fields
+    const { 
+      name, 
+      description, 
+      price, 
+      category, 
+      quantity, 
+      shipping
+    } = fields;
+
+      if(
+        !name || 
+        !description || 
+        !price || 
+        !category || 
+        !quantity || 
+        !shipping
+        ) {
+          return res.status(400).json({
+            error: " All fields are required!"
+          });
+      }
+
+    let product = req.product;
+    product = _.extend(product, fields)
+
+   
+    if(files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "image should be less than 1mb in size"
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result)
+    })
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove} = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read);
+
 router.post(
   '/product/create/:userId',
    requireSignin,
@@ -13,6 +14,7 @@ router.post(
    isAdmin, 
    create
    );
+
 router.delete(
   '/product/:productId/:userId',
   requireSignin, 
@@ -20,6 +22,14 @@ router.delete(
   isAdmin,
   remove
   );
+
+  router.put(
+    '/product/:productId/:userId',
+    requireSignin, 
+    isAuth, 
+    isAdmin,
+    update
+    );
 
 router.param('userId', userById);
 router.param('productId', productById);
