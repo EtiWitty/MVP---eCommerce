bd5b1a3ae602364f3bd9b93726d759322d2a4734
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-23 09:15:22 -0700

    created product delete

--- controllers/product.js
+++ controllers/product.js
@@ -80,3 +80,17 @@ exports.create = (req, res) => {
     })
   });
 };
+
+exports.remove = (req, res) => {
+  let product = req.product
+  product.remove((err, deleteProduct) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: 'Product deleted successfully'
+    });
+  });
+};
