5f82f200358f0652136a7fcadf82384a7ab8700f
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-03-24 20:41:43 -0700

    created back button to dashboard and handled error

--- ecommerce-front/src/admin/AddCategory.js
+++ ecommerce-front/src/admin/AddCategory.js
@@ -2,27 +2,37 @@ import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import{ Link } from 'react-router-dom';
+import {createCategory} from './apiAdmin';
 
 const AddCategory = () => {
 	const [name, setName] = useState('');
-	const [error, setErroe] = useState(false);
+	const [error, setError] = useState(false);
 	const [success, setSuccess] = useState(false);
 
 	// destructure user and token from localstorage
 	const { user, token } = isAuthenticated();
 
 	const handleChange = (e) => {
-		// setError('');
+		setError('');
 		setName(e.target.value);
 	}
 
 	const clickSubmit = (e) => {
 		e.preventDefault();
-		// setError('');
+		setError('');
 		setSuccess(false);
 
 		// make request to API to create category
-	}
+		createCategory(user._id, token, {name})
+			.then(data => {
+				if(data.error) {
+					setError(data.error);
+				} else {
+					setError("");
+					setSuccess(true);
+				}
+			});
+	};
 
 	const newCategoryForm = () => (
 		<form onSubmit={clickSubmit}>
@@ -34,11 +44,32 @@ const AddCategory = () => {
 		</form>
 	);
 
+	const showSuccess = () => {
+		if(success) {
+			return <h3 className="text-success">{name} is created</h3>
+		}
+	}
+
+	const showError = () => {
+		if(error) {
+			return <h3 className="text-danger">Category should be unique</h3>
+		}
+	};
+
+	const goBack = () => (
+		<div className="mt-5">
+			<Link to="/admin/dashboard" className="text-warning">Back to Dashboard</Link>
+		</div>
+	);
+
 	return (
-		<Layout title="Add a new category" description={ `Hello ${name}!, Ready to add a new category?`} >
+		<Layout title="Add a new category" description={ `Hello ${user.name}!, Ready to add a new category?`} >
 			<div className="row">
 				<div className="col-md-8 offset-md-2">
-					{ newCategoryForm() }
+					{ showSuccess()}
+					{showError()}
+					{newCategoryForm()}
+					{goBack()}
 				</div>
 			</div>
 		</Layout>
