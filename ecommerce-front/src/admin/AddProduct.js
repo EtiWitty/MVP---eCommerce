8dd390bb0bd4a4b99ada004c2d663e87feb566f0
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-03-25 15:57:19 -0700

    fully implemented adding product with details

--- ecommerce-front/src/admin/AddProduct.js
+++ ecommerce-front/src/admin/AddProduct.js
@@ -4,14 +4,110 @@ import { isAuthenticated } from '../auth';
 import{ Link } from 'react-router-dom';
 import { createProduct } from './apiAdmin';
 
-const { user, token} = isAuthenticated();
+
 
 const AddProduct = () => {
+	const { user, token} = isAuthenticated();
+	const [values, setValues] = useState({
+		name:'',
+		description: '',
+		price: '',
+		categories: [],
+		category: '',
+		shipping:'',
+		quantity: '',
+		photo: '',
+		loading: false,
+		error: '',
+		createProduct:'',
+		redirectToProfile: false,
+		formData:''
+	});
+
+	const {
+		name,
+		description,
+		price,
+		categories,
+		category,
+		shipping,
+		quantity,
+		loading,
+		error,
+		createProduct,
+		redirectToProfile,
+		formData
+
+	} = values;
+
+	useEffect(() => {
+		setValues({...values, formData: new FormData()})
+	}, []);
+	
+
+	const handleChange = name => event => {
+		const value = name === 'photo' ? event.target.files[0] : event.target.value;
+		formData.set(name, value);
+		setValues({...values,[name]: value});
+	}
+
+	const clickSubmit = (event) => {
+		// come back to it later
+	}
+	const newPostForm = () => (
+		<form className="mb-3" onSubmit={clickSubmit}>
+			<h4>Post Photo</h4>
+			<div className="form-group">
+				<label className="btn btn-secondary">
+					<input onChange={handleChange('photo')} type="file" name="photo" accept="image/*"/>
+				</label>
+			</div>
+
+			<div className="form-group">
+				<label>Name</label>
+				<input onChange={handleChange('name')} type="text" className="form-control" value={name}/>
+			</div>
+
+			<div className="form-group">
+				<label>Description</label>
+				<textarea onChange={handleChange('description')} type="text" className="form-control" value={description}/>
+			</div>
+
+			<div className="form-group">
+				<label>Price</label>
+				<input onChange={handleChange('price')} type="number" className="form-control" value={price}/>
+			</div>
+
+			<div className="form-group">
+				<label>Category</label>
+					<select onChange={handleChange('category')} className="form-control">
+						<option value="5e7ad0f4c2335e1aa9aed4ed">node</option>
+					</select>
+			</div>
+
+			<div className="form-group">
+				<label>Shipping</label>
+					<select onChange={handleChange('shipping')} className="form-control">
+						<option value="0">No</option>
+						<option value="1">Yes</option>
+					</select>
+			</div>
+
+			<div className="form-group">
+				<label>Quantity</label>
+				<input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity}/>
+			</div>
+
+			<button className="btn btn-outline-primary">Create Product</button>
+
+		</form>
+	)
+
 	return (
 		<Layout title="Add a new product" description={ `Hello ${user.name}!, Ready to add a new product?`} >
 			<div className="row">
 				<div className="col-md-8 offset-md-2">
-					...
+					{ newPostForm() }
 				</div>
 			</div>
 		</Layout>
