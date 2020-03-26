1df2d5393b7ba2a67acbb4dbd18d272fb83511dd
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-03-26 16:31:35 -0700

    created products with categories

--- ecommerce-front/src/admin/AddProduct.js
+++ ecommerce-front/src/admin/AddProduct.js
@@ -39,18 +39,17 @@ const AddProduct = () => {
 
 	//load categories and set data
 	const init = () => {
-		getCategories()
-			.then(data => {
-				if(data.error) {
-					setValues({...values, error: data.error});
-				} else {
-					setValues({...values, categories: data, formData: new FormData()});
-				}
-			});
+		getCategories().then(data => {
+			if(data.error) {
+				setValues({...values, error: data.error});
+			} else {
+				setValues({...values, categories: data, formData: new FormData()});
+			}
+		});
 	};
 
 	useEffect(() => {
-		init()
+		init();
 	}, []);
 	
 
@@ -136,10 +135,37 @@ const AddProduct = () => {
 		</form>
 	)
 
+	const showError = () => (
+		<div className="alert alert-danger" style={{ display: error ? '' :'none' }}>
+			{ error }
+		</div>
+	)
+
+	const showSuccess = () => (
+		<div className="alert alert-info" style={{display: createdProduct ? '' :'none'}}>
+			<h2>
+				{`${createdProduct} is created!`}
+			</h2>
+		</div>
+	)
+
+	const showLoading = () => 
+		loading && (
+			<div className="alert alert-success">
+				<h2>
+					Loading...
+				</h2>
+			</div>
+		);
+	
+
 	return (
 		<Layout title="Add a new product" description={ `Hello ${user.name}!, Ready to add a new product?`} >
 			<div className="row">
 				<div className="col-md-8 offset-md-2">
+					{ showLoading() }
+					{ showSuccess() }
+					{ showError() }
 					{ newPostForm() }
 				</div>
 			</div>
