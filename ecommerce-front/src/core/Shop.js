b3e08194bb33401635944d9a9ea2df4af77ed41c
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-04-01 21:23:25 -0700

    get categories in shop page

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from './Card';
+import { getCategories } from './apiCore';
+import { set } from "mongoose";
 
 const Shop = () => {
+	const [getCategories, setCategories] = useState([]);
+	const [getError, setError] = useState(false);
+
+	const init = () => {
+		getCategories().then(data => {
+			if(data.error) {
+				setError(data.error)
+			} else {
+				setCategories(data);
+			}
+		});
+	};
+
+	useEffect(() => {
+		init()
+	}, []);
+
 	return (
 		<Layout title="Shop Page" description="Search and find books of your choice" className="container-fluid">
 			<div className="row">
-				<div className="col-4">left sidebar</div>
+				<div className="col-4">
+					{JSON.stringify(categories)}
+				</div>
 				<div className="col-8">right sidebar</div>
 			</div>
 		</Layout>
--- ecommerce-front/src/core/ShowImage.js
+++ ecommerce-front/src/core/ShowImage.js
@@ -3,7 +3,7 @@ import { API } from '../config';
 
 const ShowImage = ({ item, url }) => (
 	<div className="product-img">
-		<img src={`${API}/${url}/photo/${item._id}`} alt={item.name} style={{maxHeight: "100%", maxWidth: "100%"}} className="bm-3"/>
+		<img src={`${API}/${url}/photo/${item._id}`} alt={item.name}  className="bm-3" style={{maxHeight: "100%", maxWidth: "100%"}}/>
 	</div>
 );
 
